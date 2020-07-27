c72d79e06f3141b44c5d1ec618494bfb624872f1
zhudayang <zhudayang@sensetime.com>
2020-07-27 14:06:13 +0800

    init project

---
+++ src/main/java/com/wustzdy/spring/boot/gradle/test/SpringBootGradleApplication.java
@@ -0,0 +1,34 @@
+package com.wustzdy.spring.boot.gradle.test;
+
+
+import com.wustzdy.spring.boot.gradle.test.service.I18nServiceImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+import java.util.Locale;
+
+@SpringBootApplication
+public class SpringBootGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGradleApplication.class, args);
+    }
+
+    @Bean
+    public I18nServiceImpl i18nService() {
+        return new I18nServiceImpl(messageSource());
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        Locale.setDefault(Locale.CHINESE);
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasenames("i18n/messages");// name of the resource bundle
+        source.setUseCodeAsDefaultMessage(true);
+        source.setDefaultEncoding("UTF-8");
+        return source;
+    }
+
+}
---
+++ src/main/java/com/wustzdy/spring/boot/gradle/test/controller/HelloJavaCoderController.java
@@ -0,0 +1,26 @@
+package com.wustzdy.spring.boot.gradle.test.controller;
+
+import com.wustzdy.spring.boot.gradle.test.service.I18nServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/api1")
+public class HelloJavaCoderController {
+    @Autowired
+    private I18nServiceImpl i18nService;
+
+    @GetMapping("/i18n/test")
+    public ResponseEntity greeting() {
+        return ResponseEntity.ok(i18nService.getMessage("message.key.hello", new Object[]{"JavaCoder"}));
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity test() {
+        return ResponseEntity.ok(i18nService.getMessage("message.key.test"));
+    }
+
+}
